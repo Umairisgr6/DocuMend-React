125c3105b28ae438bdbffb88ae4fd6d7ddef19fc
Mahnoor Aslam <141827728+mahnoor-aslam-dev@users.noreply.github.com>
2026-08-31 20:32:00 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles-utilities.css'
-// import App from './App.jsx'
-import App from './pages/Dashboard.jsx'
+import App from './App.jsx'
+// import App from './pages/Dashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
